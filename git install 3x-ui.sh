b46a95dd2063bc4e501ee119f89279979abd0f16
mohammadebrahimmahizadeh <arsin.esm1737@gmail.com>
2025-02-28 19:36:22 +0330

    Update git install 3x-ui.sh

--- git install 3x-ui.sh
+++ git install 3x-ui.sh
@@ -1,3 +1,9 @@
-git install 3x-ui
+git install 3x-ui linux
 
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+
+install certificate linux 
+
+apt-get install certbot -y
+certbot certonly --standalone --agree-tos --register-unsafely-without-email -d yourdomain.com
+certbot renew --dry-run
